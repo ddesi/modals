f1e0689df5ddbb58ca2e22da85d548b8b0693696
desi <desi.dmt@gmail.com>
2016-02-11 16:15:12 -0500

    modal assignment

---
+++ modal.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+		$(".link").click(function(){
+			$(".modal").show();
+		})	
+
+		$(".close").click(function(){
+			$(".modal").hide();
+		})	
+
+		$(".modal").click(function(){
+			$(".modal").hide();
+		})	
+
+})	
+
+
